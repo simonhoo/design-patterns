d49723c78b403bc352181b9b0312e4243aacf159
Simon.Hoo <Info@cottsoft.com>
2014-12-13 21:01:31 +0800

    抽象工厂（Abstract Factory）
    抽象工厂（Abstract Factory）

--- com.cottsoft.design.patterns/src/main/java/com/cottsoft/design/patterns/creational/abstractfactory/AbstractFactory.java
+++ com.cottsoft.design.patterns/src/main/java/com/cottsoft/design/patterns/creational/abstractfactory/AbstractFactory.java
@@ -22,20 +22,173 @@
 
 package com.cottsoft.design.patterns.creational.abstractfactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cottsoft.design.patterns.Log;
+
 /**
  * Description：<br> 
  * 抽象工厂（Abstract Factory）
  * @author  Simon.Hoo(Info@cottsoft.com)
- * @date    2005年03月03日
+ * @date    2005年04月04日
  * @version v1.0.0
  */
 public class AbstractFactory {
-
+	private Logger logger = LoggerFactory.getLogger(AbstractFactory.class);
+	
+	//定义两家工厂
+	public static PhoneFactory foxconn,samsung;
+	
+	//定义两款手机
+	public static CellPhone iphone,samsungGalaxy;
+	
+	//定议两款电话机
+	public static TelPhone siemensPhone,tclPhone;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//实例化工厂：富士康
+		foxconn = new AbstractFactory().new Foxconn();
+		//富士康生产手机：iPhone
+		iphone = foxconn.factoryCellPhone();
+		//手机工作：
+		iphone.cell();
+		
+		//富士康生产电话机：西门子电话机
+		siemensPhone = foxconn.factoryTelPhone();
+		//电话工作：
+		siemensPhone.phone();
+		
+		//实例化工厂：三星
+		samsung = new AbstractFactory().new Samsung();
+		//三星生产手机：Samsung GALAXY手机
+		samsungGalaxy = samsung.factoryCellPhone();
+		//手机工作：
+		samsungGalaxy.cell();
+		
+		//三星生产电话机：TCL电话机
+		tclPhone = samsung.factoryTelPhone();
+		//电话工作：
+		tclPhone.phone();
 	}
 
+	/**
+	 * 抽像工厂
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public interface PhoneFactory{
+		public CellPhone factoryCellPhone();
+		public TelPhone factoryTelPhone();
+	}
+	
+	/**
+	 * 抽象产品：手机
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public interface CellPhone{
+		public void cell();
+	}
+	
+	/**
+	 * 抽象产品：电话机
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public interface TelPhone{
+		public void phone();
+	}
+	
+	/**
+	 * 具体工厂：富士康
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public class Foxconn implements PhoneFactory{
+		public Foxconn(){
+			Log.log(logger, "富士康工厂生产：");
+		}
+		
+		@Override
+		public CellPhone factoryCellPhone() {
+			return new IPhone();
+		}
+
+		@Override
+		public TelPhone factoryTelPhone() {
+			return new SiemensPhone();
+		}		
+	}
+	
+	/**
+	 * 具体工厂：三星
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public class Samsung implements PhoneFactory{
+		public Samsung(){
+			Log.log(logger, "三星工厂生产：");
+		}
+		
+		@Override
+		public CellPhone factoryCellPhone() {
+			return new SamsungGalaxy();
+		}
+
+		@Override
+		public TelPhone factoryTelPhone() {
+			return new TclPhone();
+		}		
+	}
+	
+	/**
+	 * 手机实现：iPhone手机
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public class IPhone implements CellPhone{
+		@Override
+		public void cell() {
+			Log.log(logger, "Iphone 手机");
+		}
+	}
+	
+	/**
+	 * 手机实现：三星GALAXY手机
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public class SamsungGalaxy implements CellPhone{
+		@Override
+		public void cell() {
+			Log.log(logger, "Samsung GALAXY手机。");
+		}
+	}
+	
+	/**
+	 * 电话机实现：西门子电话
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public class SiemensPhone implements TelPhone{
+		@Override
+		public void phone() {
+			Log.log(logger, "西门子电话机。");
+		}		
+	}
+	
+	/**
+	 * 电话机实现：TCL电话
+	 * @author  Simon.Hoo(Info@cottsoft.com)
+	 * @version v1.0.0
+	 */
+	public class TclPhone implements TelPhone{
+		@Override
+		public void phone() {
+			Log.log(logger, "TCL电话机。");
+		}
+	}
 }
 
 
